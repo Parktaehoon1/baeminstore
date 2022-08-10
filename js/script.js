9cb19ca5e8a7620c80df6d41ce7d5c8c14ddaeb8
Parktaehoon1 <92parkth@gmail.com>
2022-08-10 22:27:15 +0900

    JSON파일로 html 밀어넣기

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
     let header = $('.header');
-    
-    $(window).scroll(function(){
+
+    $(window).scroll(function () {
         let temp = $(window).scrollTop();
-        if(temp > 0){
+        if (temp > 0) {
             header.addClass('header-fix');
         } else {
             header.removeClass('header-fix');
@@ -13,8 +13,6 @@ $(document).ready(function () {
 
 window.onload = function () {
     AOS.init();
-
-
     //     $("#wrapper").dotdotdot({
     //         wrapper  : 'div',  /*  콘텐트를 감쌀 요소. */
     //         ellipsis: '... ',  /*  말줄임표를 뭘로 할지 */
@@ -44,12 +42,91 @@ window.onload = function () {
         loop: true,
         speed: 500,
         slidesPerView: 3,
-        slidesPerGroup: 3,  
-        spaceBetween : 6,
+        slidesPerGroup: 3,
+        spaceBetween: 6,
         navigation: {
             nextEl: ".main-item-prev",
             prevEl: ".main-item-next",
         },
-
     })
+
+    // 데이터 받아보기
+    fetch("itemlist.json")
+        .then(res => res.json())
+        .then(data => {
+            // console.log(data)
+
+            let html = ``
+
+            data.forEach(element => {
+                // console.log(element)
+                html += `
+                     <div class="goods-list">
+                    <img src="${element.url}" alt="gg">
+                    <span class="goods-list-hover">
+                        <img src="images/itemlist-hover.png" alt="">
+                    </span>
+                    <div class="goods-list-desc">
+                        <p>${element.title}</p>
+                        <p>${element.title}</p>
+                    </div>
+                    </div>
+                    `;
+            });
+            html += ``;
+            document.getElementById('item-list').innerHTML = html;
+        })
+        .catch(err => console.log(err))
+
+    let dataPromise = function (_method, _url, _payload) {
+        return new Promise((resolve, reject) => {
+            let xhr = new XMLHttpRequest();
+            xhr.open(_method, _url);
+            xhr.setRequestHeader('Content-type', 'application/json');
+            let jData = JSON.stringify(_payload);
+            xhr.send(jData);
+
+            xhr.onreadystatechange = function (e) {
+                if (xhr.readyState !== XMLHttpRequest.DONE) return;
+                if (xhr.status === 200) {
+                    resolve(xhr.response);
+                    console.log(xhr.responseText)
+                } else {
+                    reject(console.log("Error"));
+                }
+            }
+        })
+    }
+    dataPromise('GET', 'itemlistmd.json')
+        .then(JSON.parse)
+        .then(show)
+        .catch(console.error);
+
+    function show(_obj) {
+        console.log('성공', _obj);
+
+        let html = ``
+
+        _obj.forEach(element => {
+            console.log(element.url)
+            html += `
+            <div class="goods-list">
+                <img src="${element.url}" alt="gg">
+                <span class="goods-list-hover">
+                <img src="images/itemlist-hover.png" alt="">
+                </span>
+                <div class="goods-list-desc">
+                <p>${element.title}</p>
+                <p>${element.title}</p>
+                </div>
+                </div>
+            `;
+        });
+        html += ``;
+        document.getElementById('md-item-list').innerHTML = html;
+
+
+    }
+
+
 }
